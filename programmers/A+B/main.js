2587e225cae39078c12b0624e9083adbe95d0c25
dixk3458 <dixk3458@naver.com>
2023-04-03 13:37:33 +0900

    A+B solution

---
+++ programmers/A+B/main.js
@@ -0,0 +1,11 @@
+function solution(num1, num2) {
+    if(num1<=-50000 && num1>=50000 && num2<=-50000 && num2>=50000){
+        return;
+    }
+    
+   let answer = num1+num2;
+    return answer;
+}
+
+
+solution(2,3);
